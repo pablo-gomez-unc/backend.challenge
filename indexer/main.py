2fa22fa62f445d8a8a271ccce9164fa41e997e1a
Pablo <pablo.gomez@unc.edu.ar>
2022-03-19 11:34:44 -0300

    Detection Consumer and Alerts Producer added

--- indexer/main.py
+++ indexer/main.py
@@ -1,26 +1,24 @@
 import os
 import json
-from time import sleep
 from kafka import KafkaProducer
 from kafka import KafkaConsumer
-from faker import Faker
 
 KAFKA_BROKER_URL = os.environ.get("KAFKA_BROKER_URL")
 DETECTIONS_TOPIC = os.environ.get("DETECTIONS_TOPIC")
 ALERTS_TOPIC = os.environ.get("ALERTS_TOPIC")
+SUSPICIOUS_VEHICLE = os.environ.get("SUSPICIOUS_VEHICLE")
 
 if __name__ == "__main__":
     consumer = KafkaConsumer(
         DETECTIONS_TOPIC,
         bootstrap_servers= KAFKA_BROKER_URL,
-        value_serializer=lambda value: json.dumps(value).encode(),
+        value_deserializer=lambda value: json.loads(value.decode('utf-8'))
     )
     producer = KafkaProducer(
         bootstrap_servers=KAFKA_BROKER_URL,
-        # Encode all values as JSON
-        value_serializer=lambda value: json.dumps(value).encode(),
+        value_serializer=lambda value: json.dumps(value).encode()
     )
     for message in consumer:
         message = message.value
-        producer.send(ALERTS_TOPIC, value=message)
-        sleep(3)
+        if message['Category'] == SUSPICIOUS_VEHICLE :
+            producer.send(ALERTS_TOPIC, value=message)
