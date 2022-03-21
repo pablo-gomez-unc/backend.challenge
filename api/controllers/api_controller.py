37900b2b1db35106ce5372b7ca56034b7f1618c0
Pablo <pablo.gomez@unc.edu.ar>
2022-03-20 23:07:59 -0300

    removed mongodb logic from controller

--- api/controllers/api_controller.py
+++ api/controllers/api_controller.py
@@ -13,20 +13,17 @@ def get_detections():
     skip_param = request.args.get("skip")  
     limit_param = request.args.get("limit")
         
-    detections = (
-        DbService.get_db().detections.find()
-        .skip (int(skip_param) if skip_param is not None else 0)
-        .limit(int(limit_param) if limit_param is not None else 0)
+    detections = DbService().get_detections(
+        int(skip_param) if skip_param is not None else 0,
+        int(limit_param) if limit_param is not None else 0
     )
-    
-    detections = list(detections)
-    [detection.pop("_id") for detection in detections]
     
     current_app.logger.debug ("Fetched Detections: %s", detections)
     return jsonify(detections)
     
 @blueprint.route('/api/v1/stats')
 def get_stats():
+    
     return "stats"
 
 @blueprint.route('/api/v1/alerts')
--- api/services/DbService.py
+++ api/services/DbService.py
@@ -1,8 +1,26 @@
+from flask_pymongo import PyMongo
 from pymongo import MongoClient
 
-class DbService :
-    @staticmethod
-    def get_db():
-        CONNECTION_STRING = "mongodb://root:masterkey@mongo:27017"
-        client = MongoClient(CONNECTION_STRING)
+class DbService (object):
+    CONNECTION_STRING = "mongodb://root:masterkey@mongo:27017"
+    db = None
+    
+    def __new__(self):
+        if not hasattr(self, 'instance'):
+            self.instance = super(DbService, self).__new__(self)
+            self.db = self.__connect()
+        return self.instance
+
+    def __connect (self):
+        client = MongoClient(self.CONNECTION_STRING)
         return client.intellisite
+        
+    def get_detections(self, skip:int = 0, limit:int = 0) -> list:    
+        detections = self.db.detections.find().skip(skip).limit(limit)
+        return [detection.pop("_id") for detection in detections]
+        
+    def get_alerts(self) -> list:
+        alerts = self.db.alerts.find()
+        return [alert.pop("_id") for alert in alerts]
+    
+    
